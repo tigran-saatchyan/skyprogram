8d7538cebf168a55569aa1794e72e09729b9e263
Tigran O. Saatchyan <mr.saatchyan@yandex.com>
2023-01-15 01:47:01 +0400

    updated .gitignore

---
+++ config.py
@@ -0,0 +1,7 @@
+# path configurations
+import os
+
+POSTS_JSON_PATH = os.path.join('data', 'posts.json')
+COMMENTS_JSON_PATH = os.path.join('data', 'comments.json')
+BOOKMARK_JSON_PATH = os.path.join('data', 'bookmarks.json')
+LOGGING_PATH = 'logs'
